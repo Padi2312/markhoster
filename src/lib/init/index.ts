84532528ba43be69e22a47c29f5130c91f464931
padi2312 <patrick.arndt.99@web.de>
2025-07-01 20:56:34 +0200

    feat: update admin user if env changes

--- src/lib/init/index.ts
+++ src/lib/init/index.ts
@@ -1,26 +1,40 @@
 import { env } from '$env/dynamic/private';
 import { db } from '$lib/server/db';
 import { users } from '$lib/server/db/schema';
-import { eq } from 'drizzle-orm';
 import argon2 from 'argon2';
+import { eq } from 'drizzle-orm';
 
 export const initAdminUser = async () => {
-    const email = env.ADMIN_EMAIL;
-    const password = env.ADMIN_PASSWORD;
-    const name = env.ADMIN_NAME ?? 'Admin';
+	const email = env.ADMIN_EMAIL;
+	const password = env.ADMIN_PASSWORD;
+	const name = 'Admin';
 
-    if (!email || !password) {
-        throw new Error('ADMIN_EMAIL and ADMIN_PASSWORD must be set in environment variables');
-    }
+	if (!email || !password) {
+		throw new Error('ADMIN_EMAIL and ADMIN_PASSWORD must be set in environment variables');
+	}
 
-    const existing = await db.select().from(users).where(eq(users.email, email)).limit(1);
-
-    if (existing.length === 0) {
-        const hashedPassword = await argon2.hash(password);
-        await db.insert(users).values({
-            email,
-            password: hashedPassword,
-            name
-        });
-    }
+	// Check if the admin user already exists if not, create it
+	// if email or password is different, update it
+	const availableUsers = await db.select().from(users);
+	if (availableUsers.length > 0) {
+		// Just take the first user as the admin user
+		const existingUser = availableUsers[0];
+		if (existingUser) {
+			await db
+				.update(users)
+				.set({
+					email,
+					password: await argon2.hash(password),
+					name
+				})
+				.where(eq(users.id, existingUser.id));
+		}
+	} else {
+		const hashedPassword = await argon2.hash(password);
+		await db.insert(users).values({
+			email,
+			password: hashedPassword,
+			name
+		});
+	}
 };
